b8514fce81d3bdad54ea785155f421033dfeb8c5
smolchanovsky <smolchanovsky@gmail.com>
2021-03-14 00:51:47 +0300

    Rename tests

--- tests/NClient.Packages/NClient.Packages.Tests/VersionTest.cs
+++ tests/NClient.Packages/NClient.Packages.Tests/VersionTest.cs
@@ -10,51 +10,51 @@ namespace NClient.Packages.Tests
     public class VersionTest
     {
         [Test]
-        public void Check_NClient() => 
+        public void NClient() => 
             PackagesVersionProvider.GetCurrent("NClient").Should().Be(PackagesVersionProvider.GetNew());
 
         [Test]
-        public void Check_NClient_AspNetProxy() => 
+        public void NClient_AspNetProxy() => 
             PackagesVersionProvider.GetCurrent("NClient.AspNetProxy").Should().Be(PackagesVersionProvider.GetNew());
 
         [Test]
-        public void Check_NClient_AspNetProxy_Standalone() => 
+        public void NClient_AspNetProxy_Standalone() => 
             PackagesVersionProvider.GetCurrent("NClient.AspNetProxy.Standalone").Should().Be(PackagesVersionProvider.GetNew());
 
         [Test]
-        public void Check_NClient_Core() => 
+        public void NClient_Core() => 
             PackagesVersionProvider.GetCurrent("NClient.Core").Should().Be(PackagesVersionProvider.GetNew());
 
         [Test]
-        public void Check_NClient_InterfaceProxy() => 
+        public void NClient_InterfaceProxy() => 
             PackagesVersionProvider.GetCurrent("NClient.InterfaceProxy").Should().Be(PackagesVersionProvider.GetNew());
 
         [Test]
-        public void Check_NClient_InterfaceProxy_Standalone() => 
+        public void NClient_InterfaceProxy_Standalone() => 
             PackagesVersionProvider.GetCurrent("NClient.InterfaceProxy.Standalone").Should().Be(PackagesVersionProvider.GetNew());
 
         [Test]
-        public void Check_NClient_Standalone() => 
+        public void NClient_Standalone() => 
             PackagesVersionProvider.GetCurrent("NClient.Standalone").Should().Be(PackagesVersionProvider.GetNew());
 
         [Test]
-        public void Check_NClient_Providers_HttpClient() => 
+        public void NClient_Providers_HttpClient() => 
             PackagesVersionProvider.GetCurrent("NClient.Providers.HttpClient").Should().Be(PackagesVersionProvider.GetNew());
 
         [Test]
-        public void Check_NClient_Providers_HttpClient_RestSharp() => 
+        public void NClient_Providers_HttpClient_RestSharp() => 
             PackagesVersionProvider.GetCurrent("NClient.Providers.HttpClient.RestSharp").Should().Be(PackagesVersionProvider.GetNew());
 
         [Test]
-        public void Check_NClient_Providers_Resilience() => 
+        public void NClient_Providers_Resilience() => 
             PackagesVersionProvider.GetCurrent("NClient.Providers.Resilience").Should().Be(PackagesVersionProvider.GetNew());
 
         [Test]
-        public void Check_NClient_Providers_Resilience_Polly() => 
+        public void NClient_Providers_Resilience_Polly() => 
             PackagesVersionProvider.GetCurrent("NClient.Providers.Resilience.Polly").Should().Be(PackagesVersionProvider.GetNew());
 
         [Test]
-        public void Check_NClient_Extensions_DependencyInjection() => 
+        public void NClient_Extensions_DependencyInjection() => 
             PackagesVersionProvider.GetCurrent("NClient.Extensions.DependencyInjection").Should().Be(PackagesVersionProvider.GetNew());
     }
 }
