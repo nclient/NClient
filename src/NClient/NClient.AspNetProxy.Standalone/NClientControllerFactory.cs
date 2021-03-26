80d8a4d5aa933007f914eb4a8add0038480b177a
smolchanovsky <smolchanovsky@gmail.com>
2021-03-27 02:23:42 +0300

    Add NClient factory

---
+++ src/NClient/NClient.AspNetProxy.Standalone/NClientControllerFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NClient.Abstractions.Clients;
+using NClient.Abstractions.HttpClients;
+using NClient.Abstractions.Resilience;
+
+namespace NClient.AspNetProxy
+{
+    [Obsolete("The right way is to add NClient controllers (see AddNClientControllers) and use INClientFactory.")]
+    public interface INClientControllerFactory
+    {
+        TInterface Create<TInterface, TController>(string host)
+            where TInterface : class, INClient
+            where TController : ControllerBase, TInterface;
+    }
+
+    [Obsolete("The right way is to add NClient controllers (see AddNClientControllers) and use NClientFactory.")]
+    public class NClientControllerFactory : INClientControllerFactory
+    {
+        private readonly IHttpClientProvider _httpClientProvider;
+        private readonly IResiliencePolicyProvider _resiliencePolicyProvider;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public NClientControllerFactory(
+            IHttpClientProvider httpClientProvider, 
+            IResiliencePolicyProvider resiliencePolicyProvider,
+            ILoggerFactory loggerFactory)
+        {
+            _httpClientProvider = httpClientProvider;
+            _resiliencePolicyProvider = resiliencePolicyProvider;
+            _loggerFactory = loggerFactory;
+        }
+
+        public TInterface Create<TInterface, TController>(string host)
+            where TInterface : class, INClient
+            where TController : ControllerBase, TInterface
+        {
+            return new NClientControllerBuilder()
+                .Use<TInterface, TController>(host, _httpClientProvider)
+                .WithResiliencePolicy(_resiliencePolicyProvider)
+                .WithLogging(_loggerFactory.CreateLogger<TInterface>())
+                .Build();
+        }
+    }
+}
