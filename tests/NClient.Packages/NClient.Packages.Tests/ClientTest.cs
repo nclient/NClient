4ffaa306904bde838e76236e9a85f477fb992ecf
smolchanovsky <smolchanovsky@gmail.com>
2021-03-28 17:23:23 +0300

    Fix client tests

--- tests/NClient.Packages/NClient.Packages.Tests/ClientTest.cs
+++ tests/NClient.Packages/NClient.Packages.Tests/ClientTest.cs
@@ -3,14 +3,12 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
-using NClient.AspNetProxy.Extensions;
-using NClient.Core;
+using NClient.Abstractions.Clients;
 using NClient.Annotations;
 using NClient.Annotations.Methods;
-using NClient.AspNetProxy;
 using NClient.Extensions.DependencyInjection;
-using NClient.InterfaceProxy;
 using NClient.Packages.Tests.Helpers;
+using NClient.Standalone;
 using NUnit.Framework;
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
@@ -53,7 +51,7 @@ namespace NClient.Packages.Tests
 
             var result = await client.GetAsync(id);
 
-            PackagesVersionProvider.GetCurrent<ControllerClientProvider>().Should().Be(PackagesVersionProvider.GetNew());
+            PackagesVersionProvider.GetCurrent<NClientBuilder>().Should().Be(PackagesVersionProvider.GetNew());
             result.Should().Be("result");
         }
 
@@ -81,7 +79,7 @@ namespace NClient.Packages.Tests
 
             var result = await client.GetAsync(id);
 
-            PackagesVersionProvider.GetCurrent<ClientProvider>().Should().Be(PackagesVersionProvider.GetNew());
+            PackagesVersionProvider.GetCurrent<NClientBuilder>().Should().Be(PackagesVersionProvider.GetNew());
             result.Should().Be("result");
         }
 
