a4e5a8f2076599d81ebb433fb051f72ebde5d2f1
smolchanovsky <smolchanovsky@gmail.com>
2021-03-28 15:15:20 +0300

    Add new packages

---
+++ src/NClient/NClient.AspNetCore/Extensions/AddNClientControllersExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Castle.DynamicProxy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.DependencyInjection;
+using NClient.AspNetProxy.Controllers;
+
+namespace NClient.AspNetProxy.Extensions
+{
+    public static class AddNClientControllersExtensions
+    {
+        private static readonly IProxyGenerator ProxyGenerator = new ProxyGenerator();
+        private static readonly IVirtualControllerRegistrar VirtualControllerRegistrar = new VirtualControllerRegistrar(ProxyGenerator);
+
+        public static IMvcCoreBuilder AddNClientControllers(this IServiceCollection serviceCollection, Action<MvcOptions>? configure = null)
+        {
+            if (serviceCollection == null)
+                throw new ArgumentNullException(nameof(serviceCollection));
+
+            var mvcCoreBuilder = serviceCollection.AddMvcCore();
+            var appAssemblies = mvcCoreBuilder.PartManager.ApplicationParts
+                .Where(x => x is AssemblyPart)
+                .Cast<AssemblyPart>()
+                .Select(x => x.Assembly);
+            var appTypes = appAssemblies.SelectMany(x => x.GetTypes());
+            var assemblyWithVirtualControllers = VirtualControllerRegistrar.Register(serviceCollection, appTypes);
+
+            var builder = mvcCoreBuilder
+                .AddApiExplorer()
+                .AddAuthorization()
+                .AddCors()
+                .AddDataAnnotations()
+                .AddFormatterMappings()
+                .AddApplicationPart(assemblyWithVirtualControllers)
+                .AddControllersAsServices()
+                .ConfigureApplicationPartManager(manager =>
+                {
+                    manager.FeatureProviders.Add(new VirtualControllerFeatureProvider(assemblyWithVirtualControllers));
+                });
+
+            if (configure != null)
+                builder.AddMvcOptions(configure);
+
+            return builder;
+        }
+    }
+}
---
+++ src/NClient/NClient.Standalone/Validators/ClientControllerValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Castle.DynamicProxy;
+using NClient.Core.Helpers;
+using NClient.Core.HttpClients;
+using NClient.Core.Interceptors;
+using NClient.Core.RequestBuilders;
+using NClient.Core.Resilience;
+using NClient.Core.Validators;
+using NClient.InterfaceProxy.Mappers;
+
+namespace NClient.AspNetProxy.Validators
+{
+    internal class ClientControllerValidator
+    {
+        public void Ensure<TInterface, TController>(IProxyGenerator proxyGenerator) 
+            where TInterface : class
+            where TController : TInterface
+        {
+            var attributeMapper = new AspNetAttributeMapper();
+
+            var requestBuilder = new RequestBuilder(
+                host: new Uri("http://localhost:5000"),
+                new RouteTemplateProvider(attributeMapper),
+                new RouteProvider(),
+                new HttpMethodProvider(attributeMapper),
+                new ParameterProvider(attributeMapper),
+                new ObjectToKeyValueConverter());
+
+            var interceptor = new ClientInterceptor<TInterface>(
+                proxyGenerator, 
+                new StubHttpClientProvider(), 
+                requestBuilder, 
+                new StubResiliencePolicyProvider(),
+                controllerType: typeof(TController));
+            
+            proxyGenerator
+                .CreateInterfaceProxyWithoutTarget<TInterface>(interceptor.ToInterceptor())
+                .EnsureValidity();
+        }
+    }
+}
