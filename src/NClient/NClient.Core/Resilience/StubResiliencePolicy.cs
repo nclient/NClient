182e2a6c19cc964dd235e6381a4d7c2d0a566c9c
smolchanovsky <smolchanovsky@gmail.com>
2021-04-17 02:34:37 +0300

    Remove policy key

--- src/NClient/NClient.Core/Resilience/StubResiliencePolicy.cs
+++ src/NClient/NClient.Core/Resilience/StubResiliencePolicy.cs
@@ -7,7 +7,7 @@ namespace NClient.Core.Resilience
 {
     internal class StubResiliencePolicy : IResiliencePolicy
     {
-        public Task<HttpResponse> ExecuteAsync(Func<Task<HttpResponse>> action, string policyKey)
+        public Task<HttpResponse> ExecuteAsync(Func<Task<HttpResponse>> action)
         {
             return action.Invoke();
         }
