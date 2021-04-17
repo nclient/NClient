5e8e5775e1a6e4823cc7ec1ba76183dacdbfaa2b
smolchanovsky <smolchanovsky@gmail.com>
2021-04-17 15:06:53 +0300

    Add aliases in sandbox

--- sandbox/NClient.Sandbox.ProxyService.Facade/Dto/WeatherForecastDto.cs
+++ sandbox/NClient.Sandbox.ProxyService.Facade/Dto/WeatherForecastDto.cs
@@ -1,17 +1,17 @@
 using System;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NClient.Sandbox.ProxyService.Facade.Dto
 {
     public class WeatherForecastDto
     {
+        [FromQuery(Name = "id")]
+        [JsonPropertyName("id")]
         public int Id { get; set; }
-
         public DateTime Date { get; set; }
-
         public int TemperatureC { get; set; }
-
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string? Summary { get; set; }
     }
 }
