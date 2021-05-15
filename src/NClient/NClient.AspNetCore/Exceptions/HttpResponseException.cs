e68793a2da8dbaa840e4d908a7805da0316fef79
smolchanovsky <smolchanovsky@gmail.com>
2021-05-15 15:36:37 +0300

    Add HttpResponceException

---
+++ src/NClient/NClient.AspNetCore/Exceptions/HttpResponseException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+
+namespace NClient.AspNetCore.Exceptions
+{
+    public class HttpResponseException : Exception
+    {
+        public HttpStatusCode Status { get; }
+        public object? Value { get; }
+        
+        public HttpResponseException(HttpStatusCode status)
+        {
+            Status = status;
+        }
+        
+        public HttpResponseException(HttpStatusCode status, object value) : this(status)
+        {
+            Value = value;
+        }
+    }
+}
