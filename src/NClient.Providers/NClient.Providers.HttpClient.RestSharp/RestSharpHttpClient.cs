2bd75f5ebb551f55eef87a053d16de1d38ba9a33
smolchanovsky <smolchanovsky@gmail.com>
2021-05-16 23:56:31 +0300

    Refactoring http clients

--- src/NClient.Providers/NClient.Providers.HttpClient.RestSharp/RestSharpHttpClient.cs
+++ src/NClient.Providers/NClient.Providers.HttpClient.RestSharp/RestSharpHttpClient.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using NClient.Abstractions.HttpClients;
+using NClient.Providers.HttpClient.RestSharp.Internals;
 using RestSharp;
 using RestSharp.Authenticators;
 using RestSharp.Serializers.SystemTextJson;
@@ -14,10 +15,14 @@ namespace NClient.Providers.HttpClient.RestSharp
 {
     public class RestSharpHttpClient : IHttpClient
     {
+        private readonly RestRequestBuilder _restRequestBuilder;
+        private readonly HttpResponseBuilder _httpResponseBuilder;
         private readonly IAuthenticator? _authenticator;
 
         public RestSharpHttpClient(IAuthenticator? authenticator = null)
         {
+            _restRequestBuilder = new RestRequestBuilder();
+            _httpResponseBuilder = new HttpResponseBuilder();
             _authenticator = authenticator;
         }
 
@@ -28,91 +33,9 @@ namespace NClient.Providers.HttpClient.RestSharp
                 Authenticator = _authenticator,
             }.UseSystemTextJson();
 
-            var restRequest = BuildRestRequest(request);
+            var restRequest = _restRequestBuilder.Build(request);
             var restResponse = await restClient.ExecuteAsync(restRequest).ConfigureAwait(false);
-            return BuildResponse(request, restResponse, bodyType, errorType);
-        }
-
-        private static IRestRequest BuildRestRequest(HttpRequest request)
-        {
-            Enum.TryParse(request.Method.ToString(), out Method method);
-            var restRequest = new RestRequest(request.Uri, method, DataFormat.Json);
-
-            foreach (var param in request.Parameters)
-            {
-                restRequest.AddParameter(param.Name, param.Value!, ParameterType.QueryString);
-            }
-
-            foreach (var header in request.Headers)
-            {
-                restRequest.AddHeader(header.Name, header.Value);
-            }
-
-            if (request.Body is not null)
-            {
-                restRequest.AddJsonBody(request.Body);
-            }
-
-            return restRequest;
-        }
-
-        private static HttpResponse BuildResponse(
-            HttpRequest request, IRestResponse restResponse, Type? bodyType = null, Type? errorType = null)
-        {
-            var response = new HttpResponse(request)
-            {
-                ContentType = string.IsNullOrEmpty(restResponse.ContentType) ? null : restResponse.ContentType,
-                ContentLength = restResponse.ContentLength,
-                ContentEncoding = string.IsNullOrEmpty(restResponse.ContentEncoding) ? null : restResponse.ContentEncoding,
-                Content = restResponse.Content,
-                StatusCode = restResponse.StatusCode,
-                ResponseUri = restResponse.ResponseUri,
-                Server = string.IsNullOrEmpty(restResponse.Server) ? null : restResponse.Server,
-                Headers = restResponse.Headers
-                    .Where(x => x.Name != null)
-                    .Select(x => new HttpHeader(x.Name!, x.Value?.ToString() ?? "")).ToArray(),
-                ErrorMessage = restResponse.ErrorMessage,
-                ErrorException = restResponse.ErrorException,
-                ProtocolVersion = restResponse.ProtocolVersion
-            };
-
-            if (bodyType is null && errorType is not null)
-            {
-                var errorObject = TryGetErrorObject(errorType, response);
-                var genericResponseType = typeof(HttpResponse<>).MakeGenericType(errorType);
-                return (HttpResponse)Activator.CreateInstance(genericResponseType, response, request, errorObject);
-            }
-            
-            if (bodyType is not null && errorType is null)
-            {
-                var bodyObject = TryGetBodyObject(bodyType, response);
-                var genericResponseType = typeof(HttpValueResponse<>).MakeGenericType(bodyType);
-                return (HttpResponse)Activator.CreateInstance(genericResponseType, response, request, bodyObject);
-            }
-            
-            if (bodyType is not null && errorType is not null)
-            {
-                var bodyObject = TryGetBodyObject(bodyType, response);
-                var errorObject = TryGetErrorObject(errorType, response);
-                var genericResponseType = typeof(HttpValueResponse<,>).MakeGenericType(bodyType, errorType);
-                return (HttpResponse)Activator.CreateInstance(genericResponseType, response, request, bodyObject, errorObject);
-            }
-            
-            return response;
-        }
-
-        private static object? TryGetBodyObject(Type bodyType, HttpResponse response)
-        {
-            return response.IsSuccessful && !string.IsNullOrEmpty(response.Content)
-                ? JsonSerializer.Deserialize(response.Content!, bodyType)
-                : null;
-        }
-        
-        private static object? TryGetErrorObject(Type errorType, HttpResponse response)
-        {
-            return !response.IsSuccessful && !string.IsNullOrEmpty(response.Content)
-                ? JsonSerializer.Deserialize(response.Content!, errorType)
-                : null;
+            return _httpResponseBuilder.Build(request, restResponse, bodyType, errorType);
         }
     }
 }
--- src/NClient.Providers/NClient.Providers.HttpClient.System/SystemHttpClient.cs
+++ src/NClient.Providers/NClient.Providers.HttpClient.System/SystemHttpClient.cs
@@ -1,64 +1,34 @@
 ﻿using System;
-using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Options;
 using NClient.Abstractions.HttpClients;
-using HttpHeader = NClient.Abstractions.HttpClients.HttpHeader;
-using HttpResponse = NClient.Abstractions.HttpClients.HttpResponse;
+using NClient.Providers.HttpClient.System.Internals;
 
 namespace NClient.Providers.HttpClient.System
 {
     public class SystemHttpClient : IHttpClient
     {
+        private readonly HttpRequestMessageBuilder _httpRequestMessageBuilder;
+        private readonly HttpResponseBuilder _httpResponseBuilder;
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly string _httpClientName;
 
         public SystemHttpClient(IHttpClientFactory httpClientFactory, string? httpClientName = null)
         {
+            _httpRequestMessageBuilder = new HttpRequestMessageBuilder();
+            _httpResponseBuilder = new HttpResponseBuilder();
             _httpClientFactory = httpClientFactory;
             _httpClientName = httpClientName ?? Options.DefaultName;
         }
 
         public async Task<HttpResponse> ExecuteAsync(HttpRequest request, Type? bodyType = null, Type? errorType = null)
         {
-            var httpRequestMessage = BuildRequestMessage(request);
+            var httpRequestMessage = _httpRequestMessageBuilder.Build(request);
             var (httpResponseMessage, exception) = await TrySendAsync(httpRequestMessage).ConfigureAwait(false);
-            return await BuildResponseAsync(request, httpResponseMessage, bodyType, errorType, exception).ConfigureAwait(false);
+            return await _httpResponseBuilder.BuildAsync(request, httpResponseMessage, bodyType, errorType, exception).ConfigureAwait(false);
         }
 
-        private static HttpRequestMessage BuildRequestMessage(HttpRequest request)
-        {
-            var parameters = request.Parameters
-                .ToDictionary(x => x.Name, x => x.Value!.ToString());
-            var uri = new Uri(QueryHelpers.AddQueryString(request.Uri.ToString(), parameters));
-
-            var httpRequestMessage = new HttpRequestMessage
-            {
-                Method = request.Method,
-                RequestUri = uri
-            };
-
-            httpRequestMessage.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
-
-            if (request.Body != null)
-            {
-                var body = JsonSerializer.Serialize(request.Body);
-                httpRequestMessage.Content = new StringContent(body, Encoding.UTF8, mediaType: "application/json");
-            }
-
-            foreach (var header in request.Headers)
-            {
-                httpRequestMessage.Headers.Add(header.Name, header.Value);
-            }
-
-            return httpRequestMessage;
-        }
-        
         private async Task<(HttpResponseMessage HttpResponseMessage, Exception? Exception)> TrySendAsync(HttpRequestMessage httpRequestMessage)
         {
             var httpClient = _httpClientFactory.CreateClient(_httpClientName);
@@ -73,73 +43,6 @@ namespace NClient.Providers.HttpClient.System
             {
                 return (httpResponseMessage, e);
             }
-        }
-
-        private static async Task<HttpResponse> BuildResponseAsync(
-            HttpRequest request, HttpResponseMessage httpResponseMessage, 
-            Type? bodyType = null, Type? errorType = null, Exception? exception = null)
-        {
-            var headers = httpResponseMessage.Headers
-                .Select(x => new HttpHeader(x.Key!, x.Value?.FirstOrDefault() ?? ""))
-                .ToArray();
-            var contentHeaders = httpResponseMessage.Content.Headers
-                .Select(x => new HttpHeader(x.Key!, x.Value?.FirstOrDefault() ?? ""))
-                .ToArray();
-            var content = await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-            var response = new HttpResponse(request)
-            {
-                ContentType = httpResponseMessage.Content.Headers.ContentType?.MediaType,
-                ContentLength = httpResponseMessage.Content.Headers.ContentLength,
-                ContentEncoding = httpResponseMessage.Content.Headers.ContentEncoding.FirstOrDefault(),
-                Content = content,
-                StatusCode = httpResponseMessage.StatusCode,
-                StatusDescription = httpResponseMessage.StatusCode.ToString(),
-                ResponseUri = httpResponseMessage.RequestMessage.RequestUri,
-                Server = httpResponseMessage.Headers.Server?.ToString(),
-                Headers = headers.Concat(contentHeaders).ToArray(),
-                ErrorMessage = exception?.Message,
-                ErrorException = exception,
-                ProtocolVersion = httpResponseMessage.Version
-            };
-
-            if (bodyType is null && errorType is not null)
-            {
-                var errorObject = TryGetErrorObject(errorType, response);
-                var genericResponseType = typeof(HttpResponse<>).MakeGenericType(errorType);
-                return (HttpResponse)Activator.CreateInstance(genericResponseType, response, request, errorObject);
-            }
-            
-            if (bodyType is not null && errorType is null)
-            {
-                var bodyObject = TryGetBodyObject(bodyType, response);
-                var genericResponseType = typeof(HttpValueResponse<>).MakeGenericType(bodyType);
-                return (HttpResponse)Activator.CreateInstance(genericResponseType, response, request, bodyObject);
-            }
-            
-            if (bodyType is not null && errorType is not null)
-            {
-                var bodyObject = TryGetBodyObject(bodyType, response);
-                var errorObject = TryGetErrorObject(errorType, response);
-                var genericResponseType = typeof(HttpValueResponse<,>).MakeGenericType(bodyType, errorType);
-                return (HttpResponse)Activator.CreateInstance(genericResponseType, response, request, bodyObject, errorObject);
-            }
-            
-            return response;
-        }
-
-        private static object? TryGetBodyObject(Type bodyType, HttpResponse response)
-        {
-            return response.IsSuccessful && !string.IsNullOrEmpty(response.Content)
-                ? JsonSerializer.Deserialize(response.Content!, bodyType)
-                : null;
-        }
-        
-        private static object? TryGetErrorObject(Type errorType, HttpResponse response)
-        {
-            return !response.IsSuccessful && !string.IsNullOrEmpty(response.Content)
-                ? JsonSerializer.Deserialize(response.Content!, errorType)
-                : null;
         }
     }
 }
