3bb299a57356c70d8deb483ba9b86a87309fb909
smolchanovsky <smolchanovsky@gmail.com>
2021-05-25 00:16:36 +0300

    Use builders

--- src/NClient.Extensions/NClient.Extensions.DependencyInjection/AddControllerBasedNClientExtensions.cs
+++ src/NClient.Extensions/NClient.Extensions.DependencyInjection/AddControllerBasedNClientExtensions.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Net.Http;
-using System.Text.Json;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NClient.Abstractions;
-using NClient.Abstractions.HttpClients;
-using Polly;
 
 namespace NClient.Extensions.DependencyInjection
 {
@@ -13,72 +10,14 @@ namespace NClient.Extensions.DependencyInjection
     public static class AddControllerBasedNClientExtensions
     {
         public static IServiceCollection AddNClient<TInterface, TController>(this IServiceCollection serviceCollection,
-            string host, JsonSerializerOptions jsonSerializerOptions, IAsyncPolicy<HttpResponse> asyncPolicy, string? httpClientName = null)
-            where TInterface : class
-            where TController : TInterface
-        {
-            return serviceCollection.AddSingleton(serviceProvider =>
-            {
-                var logger = serviceProvider.GetRequiredService<ILogger<TInterface>>();
-                var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
-
-                return new NClientBuilder()
-                    .Use<TInterface, TController>(host)
-                    .WithCustomHttpClient(httpClientFactory, httpClientName)
-                    .WithCustomSerializer(jsonSerializerOptions)
-                    .WithResiliencePolicy(asyncPolicy)
-                    .WithLogging(logger)
-                    .Build();
-            });
-        }
-
-        public static IServiceCollection AddNClient<TInterface, TController>(this IServiceCollection serviceCollection,
-            string host, JsonSerializerOptions jsonSerializerOptions, string? httpClientName = null)
-            where TInterface : class
-            where TController : TInterface
-        {
-            return serviceCollection.AddSingleton(serviceProvider =>
-            {
-                var logger = serviceProvider.GetRequiredService<ILogger<TInterface>>();
-                var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
-
-                return new NClientBuilder()
-                    .Use<TInterface, TController>(host)
-                    .WithCustomHttpClient(httpClientFactory, httpClientName)
-                    .WithCustomSerializer(jsonSerializerOptions)
-                    .WithLogging(logger)
-                    .Build();
-            });
-        }
-
-        public static IServiceCollection AddNClient<TInterface, TController>(this IServiceCollection serviceCollection,
-            string host, IAsyncPolicy<HttpResponse> asyncPolicy, string? httpClientName = null)
-            where TInterface : class
-            where TController : TInterface
-        {
-            return serviceCollection.AddSingleton(serviceProvider =>
-            {
-                var logger = serviceProvider.GetRequiredService<ILogger<TInterface>>();
-                var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
-
-                return new NClientBuilder()
-                    .Use<TInterface, TController>(host)
-                    .WithCustomHttpClient(httpClientFactory, httpClientName)
-                    .WithResiliencePolicy(asyncPolicy)
-                    .WithLogging(logger)
-                    .Build();
-            });
-        }
-
-        public static IServiceCollection AddNClient<TInterface, TController>(this IServiceCollection serviceCollection,
             string host, string? httpClientName = null)
             where TInterface : class
             where TController : TInterface
         {
             return serviceCollection.AddSingleton(serviceProvider =>
             {
-                var logger = serviceProvider.GetRequiredService<ILogger<TInterface>>();
                 var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+                var logger = serviceProvider.GetRequiredService<ILogger<TInterface>>();
 
                 return new NClientBuilder()
                     .Use<TInterface, TController>(host)
@@ -89,19 +28,43 @@ namespace NClient.Extensions.DependencyInjection
         }
 
         public static IServiceCollection AddNClient<TInterface, TController>(this IServiceCollection serviceCollection,
-            Func<INClientBuilder, IControllerBasedClientBuilder<TInterface, TController>> configure)
+            string host, Func<IControllerBasedClientBuilder<TInterface, TController>, IControllerBasedClientBuilder<TInterface, TController>> configure,
+            string? httpClientName = null)
             where TInterface : class
             where TController : TInterface
         {
-            return serviceCollection.AddSingleton(_ => configure(new NClientBuilder()).Build());
+            return serviceCollection.AddSingleton(serviceProvider =>
+            {
+                var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+                var logger = serviceProvider.GetRequiredService<ILogger<TInterface>>();
+
+                var nclientBuilder = new NClientBuilder()
+                    .Use<TInterface, TController>(host)
+                    .WithCustomHttpClient(httpClientFactory, httpClientName)
+                    .WithLogging(logger);
+
+                return configure(nclientBuilder).Build();
+            });
         }
 
         public static IServiceCollection AddNClient<TInterface, TController>(this IServiceCollection serviceCollection,
-            Func<IServiceProvider, INClientBuilder, IControllerBasedClientBuilder<TInterface, TController>> configure)
+            string host, Func<IServiceProvider, IControllerBasedClientBuilder<TInterface, TController>, IControllerBasedClientBuilder<TInterface, TController>> configure,
+            string? httpClientName = null)
             where TInterface : class
             where TController : TInterface
         {
-            return serviceCollection.AddSingleton(serviceProvider => configure(serviceProvider, new NClientBuilder()).Build());
+            return serviceCollection.AddSingleton(serviceProvider =>
+            {
+                var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+                var logger = serviceProvider.GetRequiredService<ILogger<TInterface>>();
+
+                var nclientBuilder = new NClientBuilder()
+                    .Use<TInterface, TController>(host)
+                    .WithCustomHttpClient(httpClientFactory, httpClientName)
+                    .WithLogging(logger);
+
+                return configure(serviceProvider, nclientBuilder).Build();
+            });
         }
     }
 }
