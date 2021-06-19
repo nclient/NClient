84de98cfd665f739050e79da712c5a565a4a93b4
smolchanovsky <smolchanovsky@gmail.com>
2021-06-19 19:14:23 +0300

    Add docs for handlers

--- src/NClient/NClient.Abstractions/Handling/IClientHandler.cs
+++ src/NClient/NClient.Abstractions/Handling/IClientHandler.cs
@@ -4,11 +4,25 @@ using NClient.Abstractions.Invocation;
 
 namespace NClient.Abstractions.Handling
 {
+    /// <summary>
+    /// Provides custom functionality to handling HTTP requests and responses.
+    /// </summary>
     public interface IClientHandler
     {
-        Task<HttpRequest> HandleRequestAsync(
-            HttpRequest httpRequest, MethodInvocation methodInvocation);
-        Task<HttpResponse> HandleResponseAsync(
-            HttpResponse httpResponse, MethodInvocation methodInvocation);
+        /// <summary>
+        /// Handles HTTP request before sending it.
+        /// </summary>
+        /// <param name="httpRequest">The source HTTP request.</param>
+        /// <param name="methodInvocation">The information about invocation of a client method.</param>
+        /// <returns></returns>
+        Task<HttpRequest> HandleRequestAsync(HttpRequest httpRequest, MethodInvocation methodInvocation);
+
+        /// <summary>
+        /// Handles HTTP response after receiving it.
+        /// </summary>
+        /// <param name="httpResponse">The source HTTP response.</param>
+        /// <param name="methodInvocation">The information about invocation of a client method.</param>
+        /// <returns></returns>
+        Task<HttpResponse> HandleResponseAsync(HttpResponse httpResponse, MethodInvocation methodInvocation);
     }
 }
--- src/NClient/NClient.Abstractions/Invocation/MethodInvocation.cs
+++ src/NClient/NClient.Abstractions/Invocation/MethodInvocation.cs
@@ -3,15 +3,32 @@ using System.Reflection;
 
 namespace NClient.Abstractions.Invocation
 {
+    /// <summary>
+    /// Encapsulates an information about invocation of a client method.
+    /// </summary>
     public class MethodInvocation
     {
+        /// <summary>
+        /// Gets type of client interface.
+        /// </summary>
         public Type ClientType { get; }
+
+        /// <summary>
+        /// Gets information about the client method. 
+        /// </summary>
         public MethodInfo MethodInfo { get; }
+
+        /// <summary>
+        /// Gets values of the arguments in the client method call. 
+        /// </summary>
         public object[] MethodArguments { get; }
+
+        /// <summary>
+        /// Gets type returned by the client method.
+        /// </summary>
         public Type ResultType { get; }
 
-        public MethodInvocation(
-            Type clientType, MethodInfo methodInfo, object[] methodArguments, Type resultType)
+        public MethodInvocation(Type clientType, MethodInfo methodInfo, object[] methodArguments, Type resultType)
         {
             ClientType = clientType;
             MethodInfo = methodInfo;
