86e6596087a45dd51f47d0b3a3df8c27a7c1fba3
smolchanovsky <smolchanovsky@gmail.com>
2021-06-22 03:37:55 +0300

    Fix resilience policy for controllers

---
+++ src/NClient/NClient.Core/Helpers/MethodInfoEqualityComparer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace NClient.Core.Helpers
+{
+    internal class MethodInfoEqualityComparer : IEqualityComparer<MethodInfo>
+    {
+        public bool Equals(MethodInfo left, MethodInfo right)
+        {
+            if (ReferenceEquals(left, right)) 
+                return true;
+            if (left.GetType() != right.GetType()) 
+                return false;
+            
+            return left.MemberType == right.MemberType 
+                   && left.Name == right.Name 
+                   && left.GetParameters().Select(x => x.ParameterType)
+                       .SequenceEqual(right.GetParameters().Select(x => x.ParameterType))
+                   && left.ReturnType == right.ReturnType;
+        }
+
+        public int GetHashCode(MethodInfo obj)
+        {
+            unchecked
+            {
+                var hashCode = (int) obj.MemberType;
+                hashCode = (hashCode * 397) ^ obj.Name.GetHashCode();
+                hashCode = (hashCode * 397) ^ obj.ReturnType.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}
