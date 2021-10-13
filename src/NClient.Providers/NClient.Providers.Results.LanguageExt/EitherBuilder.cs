b7736a1083e88854c23f4aa012c78c66e27e054d
smolchanovsky <smolchanovsky@gmail.com>
2021-10-14 01:39:37 +0300

    Add NClient.Providers.Results.LanguageExt

---
+++ src/NClient.Providers/NClient.Providers.Results.LanguageExt/EitherBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using LanguageExt;
+using LanguageExt.DataTypes.Serialisation;
+using NClient.Abstractions.HttpClients;
+using NClient.Abstractions.Results;
+using NClient.Abstractions.Serialization;
+
+namespace NClient.Providers.Results.LanguageExt
+{
+    public class EitherBuilder : IResultBuilder<IHttpResponse>
+    {
+        public bool CanBuild(Type resultType, IHttpResponse response)
+        {
+            if (!resultType.IsGenericType)
+                return false;
+
+            return resultType.GetGenericTypeDefinition() == typeof(Either<,>);
+        }
+        
+        public object? Build(Type resultType, IHttpResponse response, ISerializer serializer)
+        {
+            if (response.IsSuccessful)
+            {
+                var value = serializer.Deserialize(response.Content.ToString(), resultType.GetGenericArguments()[1]);
+                return BuildEither(resultType.GetGenericArguments()[0], left: null, resultType.GetGenericArguments()[1], right: value);
+            }
+            
+            var error = serializer.Deserialize(response.Content.ToString(), resultType.GetGenericArguments()[0]);
+            return BuildEither(resultType.GetGenericArguments()[0], left: error, resultType.GetGenericArguments()[1], right: null);
+        }
+
+        private object? BuildEither(Type leftType, object? left, Type rightType, object? right)
+        {
+            var eitherData = BuildEitherData(leftType, left, rightType, right);
+            var eitherType = typeof(Either<,>).MakeGenericType(leftType, rightType);
+            var arrayOfEitherData = Array.CreateInstance(eitherData.GetType(), length: 1);
+            arrayOfEitherData.SetValue(eitherData, index: 0);
+            return Activator.CreateInstance(eitherType, arrayOfEitherData);
+        }
+        
+        private object BuildEitherData(Type leftType, object? left, Type rightType, object? right)
+        {
+            if (left is not null)
+            {
+                var rightMethodInfo = typeof(EitherData).GetMethod("Left")!.MakeGenericMethod(leftType, rightType);
+                return rightMethodInfo.Invoke(obj: null, new[] { left });
+            }
+            
+            if (right is not null)
+            {
+                var rightMethodInfo = typeof(EitherData).GetMethod("Right")!.MakeGenericMethod(leftType, rightType);
+                return rightMethodInfo.Invoke(obj: null, new[] { right });
+            }
+
+            throw new ArgumentException($"Either left or right is expected. But the right is {right?.GetType().Name ?? "null"}, and the left is {left?.GetType().Name ?? "null"}.");
+        }
+    }
+}
