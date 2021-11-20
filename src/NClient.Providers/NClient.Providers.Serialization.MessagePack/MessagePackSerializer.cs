2ded881c1d488d9f91f548a7b60deb68d56225dc
Kingmidas74 <kingmidas1992@gmail.com>
2021-11-20 22:54:40 +0300

    Implement MP support #298

--- src/NClient.Providers/NClient.Providers.Serialization.MessagePack/MessagePackSerializer.cs
+++ src/NClient.Providers/NClient.Providers.Serialization.MessagePack/MessagePackSerializer.cs
@@ -1,6 +1,37 @@
+using System;
+using System.Text;
+using NClient.Common.Helpers;
+using MP = MessagePack;
+
 namespace NClient.Providers.Serialization.MessagePack
 {
-    public class MessagePackSerializer
+    internal class MessagePackSerializer : ISerializer
     {
+        private readonly MessagePackSerializerSettings _messagePackSerializerSettings;
+
+        //INFO: https://github.com/msgpack/msgpack/issues/194
+        public string ContentType { get; } = "application/msgpack";
+
+        public MessagePackSerializer(MessagePackSerializerSettings messagePackSerializerSettings)
+        {
+            Ensure.IsNotNull(messagePackSerializerSettings, nameof(messagePackSerializerSettings));
+            Ensure.IsNotNullOrEmpty(messagePackSerializerSettings.ContentTypeHeader, nameof(messagePackSerializerSettings.ContentTypeHeader));
+            
+            _messagePackSerializerSettings = messagePackSerializerSettings;
+            ContentType = messagePackSerializerSettings.ContentTypeHeader;
+        }
+
+        public object? Deserialize(string source, Type returnType)
+        {
+            Ensure.IsNotNull(source, nameof(source));
+            Ensure.IsNotNull(returnType, nameof(returnType));
+
+            return MP.MessagePackSerializer.Typeless.Deserialize(Encoding.UTF8.GetBytes(source), _messagePackSerializerSettings.Options);
+        }
+
+        public string Serialize<T>(T? value)
+        {
+            return Encoding.UTF8.GetString(MP.MessagePackSerializer.Serialize(value, _messagePackSerializerSettings.Options));
+        }
     }
 }
