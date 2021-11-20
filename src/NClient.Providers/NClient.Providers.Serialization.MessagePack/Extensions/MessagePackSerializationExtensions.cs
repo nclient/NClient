29bb2f8ac74e83468340db89596e802713cc96fd
Kingmidas74 <kingmidas1992@gmail.com>
2021-11-20 22:54:54 +0300

    Add NClient extensions #298

--- src/NClient.Providers/NClient.Providers.Serialization.MessagePack/Extensions/MessagePackSerializationExtensions.cs
+++ src/NClient.Providers/NClient.Providers.Serialization.MessagePack/Extensions/MessagePackSerializationExtensions.cs
@@ -1,6 +1,45 @@
+using NClient.Common.Helpers;
+
 namespace NClient.Providers.Serialization.MessagePack.Extensions
 {
-    public class MessagePackSerializationExtensions
+    public static class MessagePackSerializationExtensions
     {
+        public static INClientOptionalBuilder<TClient, TRequest, TResponse> WithNewtonsoftJsonSerialization<TClient, TRequest, TResponse>(
+            this INClientOptionalBuilder<TClient, TRequest, TResponse> optionalBuilder) 
+            where TClient : class
+        {
+            Ensure.IsNotNull(optionalBuilder, nameof(optionalBuilder));
+            
+            return optionalBuilder.WithCustomSerialization(new MessagePackSerializerProvider());
+        }
+
+        public static INClientOptionalBuilder<TClient, TRequest, TResponse> WithNewtonsoftJsonSerialization<TClient, TRequest, TResponse>(
+            this INClientOptionalBuilder<TClient, TRequest, TResponse> optionalBuilder,
+            MessagePackSerializerSettings messagePackSerializerSettings)
+            where TClient : class
+        {
+            Ensure.IsNotNull(optionalBuilder, nameof(optionalBuilder));
+            Ensure.IsNotNull(messagePackSerializerSettings, nameof(messagePackSerializerSettings));
+            
+            return optionalBuilder.WithCustomSerialization(new MessagePackSerializerProvider(messagePackSerializerSettings));
+        }
+
+        public static INClientFactoryOptionalBuilder<TRequest, TResponse> WithNewtonsoftJsonSerialization<TRequest, TResponse>(
+            this INClientFactoryOptionalBuilder<TRequest, TResponse> optionalBuilder)
+        {
+            Ensure.IsNotNull(optionalBuilder, nameof(optionalBuilder));
+            
+            return optionalBuilder.WithCustomSerialization(new MessagePackSerializerProvider());
+        }
+
+        public static INClientFactoryOptionalBuilder<TRequest, TResponse> WithNewtonsoftJsonSerialization<TRequest, TResponse>(
+            this INClientFactoryOptionalBuilder<TRequest, TResponse> optionalBuilder,
+            MessagePackSerializerSettings messagePackSerializerSettings)
+        {
+            Ensure.IsNotNull(optionalBuilder, nameof(optionalBuilder));
+            Ensure.IsNotNull(messagePackSerializerSettings, nameof(messagePackSerializerSettings));
+            
+            return optionalBuilder.WithCustomSerialization(new MessagePackSerializerProvider(messagePackSerializerSettings));
+        }
     }
 }
--- src/NClient.Providers/NClient.Providers.Serialization.MessagePack/Extensions/UsingMessagePackSerializerExtensions.cs
+++ src/NClient.Providers/NClient.Providers.Serialization.MessagePack/Extensions/UsingMessagePackSerializerExtensions.cs
@@ -1,6 +1,65 @@
+using NClient.Common.Helpers;
+
 namespace NClient.Providers.Serialization.MessagePack.Extensions
 {
-    public class UsingMessagePackSerializerExtensions
+    public static class UsingMessagePackSerializerExtensions
     {
+        /// <summary>
+        /// Sets Newtonsoft.Json based <see cref="ISerializerProvider"/> used to create instance of <see cref="ISerializer"/>.
+        /// </summary>
+        /// <param name="serializationBuilder"></param>
+        public static INClientOptionalBuilder<TClient, TRequest, TResponse> UsingNewtonsoftJsonSerialization<TClient, TRequest, TResponse>(
+            this INClientSerializationBuilder<TClient, TRequest, TResponse> serializationBuilder)
+            where TClient : class
+        {
+            Ensure.IsNotNull(serializationBuilder, nameof(serializationBuilder));
+
+            return serializationBuilder.UsingCustomSerializer(new MessagePackSerializerProvider());
+        }
+
+        /// <summary>
+        /// Sets Newtonsoft.Json based <see cref="ISerializerProvider"/> used to create instance of <see cref="ISerializer"/>.
+        /// </summary>
+        /// <param name="serializationBuilder"></param>
+        /// <param name="messagePackSerializerSettings">The settings to be used with <see cref="MessagePackSerializer"/>.</param>
+        public static INClientOptionalBuilder<TClient, TRequest, TResponse> UsingNewtonsoftJsonSerialization<TClient, TRequest, TResponse>(
+            this INClientSerializationBuilder<TClient, TRequest, TResponse> serializationBuilder,
+            MessagePackSerializerSettings messagePackSerializerSettings)
+            where TClient : class
+        {
+            Ensure.IsNotNull(serializationBuilder, nameof(serializationBuilder));
+            Ensure.IsNotNull(messagePackSerializerSettings, nameof(messagePackSerializerSettings));
+
+            return serializationBuilder
+                .UsingCustomSerializer(new MessagePackSerializerProvider(messagePackSerializerSettings));
+        }
+
+        /// <summary>
+        /// Sets Newtonsoft.Json based <see cref="ISerializerProvider"/> used to create instance of <see cref="ISerializer"/>.
+        /// </summary>
+        /// <param name="serializationBuilder"></param>
+        public static INClientFactoryOptionalBuilder<TRequest, TResponse> UsingNewtonsoftJsonSerialization<TRequest, TResponse>(
+            this INClientFactorySerializationBuilder<TRequest, TResponse> serializationBuilder)
+        {
+            Ensure.IsNotNull(serializationBuilder, nameof(serializationBuilder));
+
+            return serializationBuilder.UsingCustomSerializer(new MessagePackSerializerProvider());
+        }
+
+        /// <summary>
+        /// Sets Newtonsoft.Json based <see cref="ISerializerProvider"/> used to create instance of <see cref="ISerializer"/>.
+        /// </summary>
+        /// <param name="serializationBuilder"></param>
+        /// <param name="messagePackSerializerSettings">The settings to be used with <see cref="MessagePackSerializer"/>.</param>
+        public static INClientFactoryOptionalBuilder<TRequest, TResponse> UsingNewtonsoftJsonSerialization<TRequest, TResponse>(
+            this INClientFactorySerializationBuilder<TRequest, TResponse> serializationBuilder,
+            MessagePackSerializerSettings messagePackSerializerSettings)
+        {
+            Ensure.IsNotNull(serializationBuilder, nameof(serializationBuilder));
+            Ensure.IsNotNull(messagePackSerializerSettings, nameof(messagePackSerializerSettings));
+
+            return serializationBuilder
+                .UsingCustomSerializer(new MessagePackSerializerProvider(messagePackSerializerSettings));
+        }
     }
 }
