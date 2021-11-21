ae7bb7feb797e13c95480b0eea71b224065b0134
Kingmidas74 <kingmidas1992@gmail.com>
2021-11-21 11:00:28 +0300

    Extrude converters to common lib

---
+++ src/NClient/NClient.Common/Helpers/Converters.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace NClient.Common.Helpers
+{
+    public static class Converters
+    {
+        public static string GetString(byte[] source)
+        {
+            var chars = new char[source.Length];
+            Buffer.BlockCopy(source, 0, chars, 0, source.Length);
+            return new string(chars);
+        }
+
+        public static byte[] GetBytes(string source)
+        {
+            byte[] result = new byte[source.Length];
+            Buffer.BlockCopy(source.ToCharArray(), 0, result, 0, result.Length);
+            return result;
+        }
+    }
+}
