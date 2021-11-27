4aa79b820887906fb8f28c58d07175a473509d03
smolchanovsky <smolchanovsky@gmail.com>
2021-11-28 01:05:03 +0300

    Add tests

--- tests/NClient/NClient.Tests/ClientTests/TimeoutClientTest.cs
+++ tests/NClient/NClient.Tests/ClientTests/TimeoutClientTest.cs
@@ -48,5 +48,35 @@ namespace NClient.Tests.ClientTests
                 .Should()
                 .ThrowExactlyAsync<TaskCanceledException>();
         }
+        
+        [Test]
+        public void TimeoutClient_GetWithTimeout_ThrowTaskCanceledException()
+        {
+            const int id = 1;
+            using var api = TimeoutApiMockFactory.MockGetMethod(id);
+            var nclient = NClientGallery.Clients.GetRest()
+                .For<ITimeoutClientWithMetadata>(api.Urls.First())
+                .WithTimeout(1.Microseconds())
+                .Build();
+
+            nclient.Invoking(x => x.Get(id))
+                .Should()
+                .ThrowExactly<TaskCanceledException>();
+        }
+        
+        [Test]
+        public async Task TimeoutClient_GetAsyncWithTimeout_ThrowTaskCanceledException()
+        {
+            const int id = 1;
+            using var api = TimeoutApiMockFactory.MockGetMethod(id);
+            var nclient = NClientGallery.Clients.GetRest()
+                .For<ITimeoutClientWithMetadata>(api.Urls.First())
+                .WithTimeout(1.Microseconds())
+                .Build();
+
+            await nclient.Invoking(x => x.GetAsync(id))
+                .Should()
+                .ThrowExactlyAsync<TaskCanceledException>();
+        }
     }
 }
