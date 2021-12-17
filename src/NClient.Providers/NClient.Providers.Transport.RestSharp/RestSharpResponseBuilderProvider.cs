77b605353114a99495c0ff66952f0e2ca10bcae2
smolchanovsky <smolchanovsky@gmail.com>
2021-12-18 01:07:14 +0300

    Remove final response builders

--- src/NClient.Providers/NClient.Providers.Transport.RestSharp/RestSharpResponseBuilderProvider.cs
+++ src/NClient.Providers/NClient.Providers.Transport.RestSharp/RestSharpResponseBuilderProvider.cs
@@ -1,21 +1,12 @@
-﻿using NClient.Providers.Transport.RestSharp.Builders;
-using NClient.Providers.Transport.RestSharp.Helpers;
-using RestSharp;
+﻿using RestSharp;
 
 namespace NClient.Providers.Transport.RestSharp
 {
     public class RestSharpResponseBuilderProvider : IResponseBuilderProvider<IRestRequest, IRestResponse>
     {
-        private readonly IRestSharpMethodMapper _restSharpMethodMapper;
-
-        public RestSharpResponseBuilderProvider()
-        {
-            _restSharpMethodMapper = new RestSharpMethodMapper();
-        }
-        
         public IResponseBuilder<IRestRequest, IRestResponse> Create(IToolset toolset)
         {
-            return new RestSharpResponseBuilder(new FinalHttpRequestBuilder(_restSharpMethodMapper));
+            return new RestSharpResponseBuilder();
         }
     }
 }
