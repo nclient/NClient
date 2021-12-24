e6f77d739b3a3af85ab571678a81f1191d3e064f
Kingmidas74 <kingmidas1992@gmail.com>
2021-12-24 21:06:42 +0300

    Add TimeoutAttribute

---
+++ src/NClient/NClient.Annotations/Operations/TimeoutAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace NClient.Annotations
+{
+    /// <summary>
+    /// Identifies an action that restrict by timeout.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class TimeoutAttribute : Attribute
+    {
+        public int Order { get; set; }
+        
+        private double Timeout { get; }
+        
+        /// <summary>
+        /// Creates a new <see cref="TimeoutAttribute"/> with the given timespan value.
+        /// </summary>
+        /// <param name="timeSpan">The timeout value</param>
+        public TimeoutAttribute(TimeSpan timeSpan)
+        {
+            Timeout = timeSpan.TotalSeconds;
+        }
+    }
+}
