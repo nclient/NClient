a73c2a5c65beb2dbc1b9ac6bb84b3b88e876e443
Kingmidas74 <kingmidas1992@gmail.com>
2022-01-04 22:15:09 +0300

    Separate tests

--- tests/NClient.Providers/NClient.Providers.Transport.SystemNetHttp.Tests/ResponseDeconstructTest.cs
+++ tests/NClient.Providers/NClient.Providers.Transport.SystemNetHttp.Tests/ResponseDeconstructTest.cs
@@ -2,13 +2,12 @@ using System;
 using AutoFixture;
 using AutoFixture.AutoMoq;
 using FluentAssertions;
-using NClient.Providers.Results.HttpResults;
 using NUnit.Framework;
 
-namespace NClient.Providers.Mapping.HttpResponses.Tests
+namespace NClient.Providers.Transport.SystemNetHttp.Tests
 {
     [Parallelizable]
-    public class HttpResponseDeconstructTest
+    public class ResponseDeconstructTest
     {
         // ReSharper disable once InconsistentNaming
         private static readonly Fixture _fixture = new();
@@ -22,25 +21,25 @@ namespace NClient.Providers.Mapping.HttpResponses.Tests
         }
 
         [Test]
-        public void IHttpResponseWithDataAndError_Deconstruct()
+        public void IResponseWithDataAndError_Deconstruct()
         {
             var resultData = _fixture.Create<Int32>();
             var error = _fixture.Create<string>();
-            var response = new HttpResponseWithError<Int32, string>(_fixture.Create<HttpResponse>(), resultData, error);
-            
-            var (data, err, _) = response;
+            var response = new ResponseWithError<Int32, string>(_fixture.Build<Response>().Create(), _fixture.Build<Request>().Create(), resultData, error);
+
+            var (data, err) = response;
             data.Should().Be(resultData);
             err.Should().Be(error);
         }
 
         [Test]
-        public void IHttpResponseWithData_Deconstruct()
+        public void IResponseWithData_Deconstruct()
         {
             var resultData = _fixture.Create<Int32>();
-            var response = new HttpResponse<Int32>(_fixture.Build<HttpResponse>().Create(), resultData);
+            var response = new Response<Int32>(_fixture.Build<Response>().Create(), _fixture.Build<Request>().Create(), resultData);
 
-            var (data, _) = response;
-            data.Should().Be(resultData);
+            var (responseData, _) = response;
+            responseData.Should().Be(resultData);
         }
     }
 }
