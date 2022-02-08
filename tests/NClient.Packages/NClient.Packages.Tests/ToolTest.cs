eb0ff3275bd33fad0ebe5c8b98f5da2613cbee3d
smolchanovsky <smolchanovsky@gmail.com>
2022-02-09 01:09:53 +0300

    Add ToolTest

---
+++ tests/NClient.Packages/NClient.Packages.Tests/ToolTest.cs
@@ -0,0 +1,61 @@
+﻿#if NET5_0
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using NUnit.Framework;
+
+namespace NClient.Packages.Tests
+{
+    public class ToolTest
+    {
+        [Test]
+        public async Task Install()
+        {
+            var installResult = await ExecuteBashCommandAsync(
+                command: "dotnet tool install --global dotnet-nclient", 
+                timeout: 10.Seconds());
+            
+            installResult.Error.Should().BeEmpty();
+            
+            var gettingVersionResult = await ExecuteBashCommandAsync(
+                command: "dotnet nclient --version",
+                timeout: 5.Seconds());
+            
+            gettingVersionResult.Error.Should().BeEmpty();
+            gettingVersionResult.Output.Should().MatchRegex("NClient.DotNetTool *.");
+        }
+        
+        private async Task<(string? Output, string? Error)> ExecuteBashCommandAsync(string command, TimeSpan timeout)
+        {
+            using var cancellationTokenSource = new CancellationTokenSource(timeout);
+            var isWindowsOs = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = isWindowsOs ? "cmd.exe" : "/bin/bash",
+                    Arguments = isWindowsOs ? "/C \"" + command + "\"" : "-c \"" + command + "\"",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false
+                }
+            };
+            
+            process.Start();
+            await process.WaitForExitAsync(cancellationTokenSource.Token);
+
+            var output = await process.StandardOutput.ReadToEndAsync();
+            Console.WriteLine(output);
+            var error = await process.StandardError.ReadToEndAsync();
+            Console.WriteLine(error);
+            return (output, error);
+        }
+    }
+}
+#endif
