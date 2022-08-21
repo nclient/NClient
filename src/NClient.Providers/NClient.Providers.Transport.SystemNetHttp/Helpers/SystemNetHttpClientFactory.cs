b20aaf99691febb96f9c77dd1166935951deb0b4
smolchanovsky <smolchanovsky@gmail.com>
2022-08-21 15:46:17 +0300

    Use SocketsHttpHandler

--- src/NClient.Providers/NClient.Providers.Transport.SystemNetHttp/Helpers/SystemNetHttpClientFactory.cs
+++ src/NClient.Providers/NClient.Providers.Transport.SystemNetHttp/Helpers/SystemNetHttpClientFactory.cs
@@ -1,11 +1,28 @@
 ﻿using System.Net.Http;
+using System.Threading;
 
 namespace NClient.Providers.Transport.SystemNetHttp.Helpers
 {
     internal class SystemNetHttpClientFactory : IHttpClientFactory
     {
+        #if NETCOREAPP2_1_OR_GREATER
+        private const bool DisposeHandler = false;
+        private static readonly HttpClientHandler? HttpHandler = new SocketsHttpHandler();
+        #else
+        private const bool DisposeHandler = true;
+        private static readonly HttpClientHandler? HttpHandler = new();
+        #endif
+        
         private readonly HttpClient _httpClient;
 
+        public SystemNetHttpClientFactory()
+        {
+            _httpClient = new HttpClient(HttpHandler, DisposeHandler)
+            {
+                Timeout = Timeout.InfiniteTimeSpan
+            };
+        }
+        
         public SystemNetHttpClientFactory(HttpClient httpClient)
         {
             _httpClient = httpClient;
