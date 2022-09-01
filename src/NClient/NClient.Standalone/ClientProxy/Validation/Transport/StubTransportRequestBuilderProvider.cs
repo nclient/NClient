ad9fe9de7c36c88dc430cb18a45182a141905c13
Reaver330 <raf.patton@outlook.com>
2022-09-01 13:56:24 +0200

    Made the stubs generic and added pipeline cancellation where needed and renamed StubTransport to StubTransportBuilder to follow the naming of the other stubs.

--- src/NClient/NClient.Standalone/ClientProxy/Validation/Transport/StubTransportRequestBuilderProvider.cs
+++ src/NClient/NClient.Standalone/ClientProxy/Validation/Transport/StubTransportRequestBuilderProvider.cs
@@ -1,13 +1,20 @@
 ﻿using NClient.Providers;
 using NClient.Providers.Transport;
+using NClient.Providers.Transport.Common;
 
 namespace NClient.Standalone.ClientProxy.Validation.Transport
 {
-    internal class StubTransportRequestBuilderProvider : ITransportRequestBuilderProvider<IRequest, IResponse>
+    internal class StubTransportRequestBuilderProvider<TRequest, TResponse> : ITransportRequestBuilderProvider<IRequest, IResponse>
     {
         public ITransportRequestBuilder<IRequest, IResponse> Create(IToolset toolset)
         {
-            return new StubTransportRequestBuilder();
+            return new StubTransportRequestBuilder<TRequest, TResponse>();
+        }
+
+        public ITransportRequestBuilder<IRequest, IResponse> Create(IToolset toolset, IPipelineCanceller pipelineCanceller)
+        {
+            pipelineCanceller.Cancel();
+            return new StubTransportRequestBuilder<TRequest, TResponse>();
         }
     }
 }
