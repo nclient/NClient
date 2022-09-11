b7505c75de4f325cf3723a711c973915736746be
Denis Suleymanov <kingmidas1992@gmail.com>
2022-09-11 12:48:46 +0300

    Add options to change DI lifetime for NClient

--- src/NClient.Extensions/NClient.Extensions.DependencyInjection/Extensions/AddCustomNClientGeneralExtensions.cs
+++ src/NClient.Extensions/NClient.Extensions.DependencyInjection/Extensions/AddCustomNClientGeneralExtensions.cs
@@ -5,98 +5,118 @@ using NClient.Common.Helpers;
 // ReSharper disable once CheckNamespace
 namespace NClient.Extensions.DependencyInjection
 {
-    public static class AddCustomNClientExtensions
+    public static partial class AddCustomNClientExtensions
     {
         /// <summary>Adds a NClient client to the DI container.</summary>
         /// <param name="serviceCollection"></param>
         /// <param name="host">The base address of URI used when sending requests.</param>
         /// <param name="implementationFactory">The action to create client with builder.</param>
+        /// <param name="serviceLifetime">Specifies the lifetime of a service in an <see cref="IServiceCollection"/></param>
         /// <typeparam name="TClient">The type of interface used to create the client.</typeparam>
         public static IServiceCollection AddCustomNClient<TClient>(this IServiceCollection serviceCollection, 
-            string host, Func<INClientApiBuilder<TClient>, TClient> implementationFactory)
+            string host, Func<INClientApiBuilder<TClient>, TClient> implementationFactory,
+            ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
             where TClient : class
         {
             Ensure.IsNotNull(serviceCollection, nameof(serviceCollection));
             Ensure.IsNotNull(host, nameof(host));
             Ensure.IsNotNull(implementationFactory, nameof(implementationFactory));
             
-            return AddCustomNClient(serviceCollection, new Uri(host), implementationFactory);
+            return AddCustomNClient(serviceCollection, new Uri(host), implementationFactory, serviceLifetime);
         }
         
         /// <summary>Adds a NClient client to the DI container.</summary>
         /// <param name="serviceCollection"></param>
         /// <param name="host">The base address of URI used when sending requests.</param>
         /// <param name="implementationFactory">The action to create client with builder.</param>
+        /// <param name="serviceLifetime">Specifies the lifetime of a service in an <see cref="IServiceCollection"/></param>
         /// <typeparam name="TClient">The type of interface used to create the client.</typeparam>
         public static IServiceCollection AddCustomNClient<TClient>(this IServiceCollection serviceCollection, 
-            Uri host, Func<INClientApiBuilder<TClient>, TClient> implementationFactory)
+            Uri host, Func<INClientApiBuilder<TClient>, TClient> implementationFactory,
+            ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
             where TClient : class
         {
             Ensure.IsNotNull(serviceCollection, nameof(serviceCollection));
             Ensure.IsNotNull(host, nameof(host));
             Ensure.IsNotNull(implementationFactory, nameof(implementationFactory));
             
-            return serviceCollection.AddSingleton(_ => implementationFactory(new NClientBuilder().For<TClient>(host)));
+            var serviceDescriptor = new ServiceDescriptor(typeof(TClient), _ => implementationFactory(new NClientBuilder().For<TClient>(host)), serviceLifetime);
+            serviceCollection.Add(serviceDescriptor);
+            return serviceCollection;
         }
         
         /// <summary>Adds a NClient client to the DI container.</summary>
         /// <param name="serviceCollection"></param>
         /// <param name="host">The base address of URI used when sending requests.</param>
         /// <param name="implementationFactory">The action to create client with builder.</param>
+        /// <param name="serviceLifetime">Specifies the lifetime of a service in an <see cref="IServiceCollection"/></param>
         /// <typeparam name="TClient">The type of interface used to create the client.</typeparam>
         public static IServiceCollection AddCustomNClient<TClient>(this IServiceCollection serviceCollection, 
-            string host, Func<IServiceProvider, INClientApiBuilder<TClient>, TClient> implementationFactory)
+            string host, Func<IServiceProvider, INClientApiBuilder<TClient>, TClient> implementationFactory,
+            ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
             where TClient : class
         {
             Ensure.IsNotNull(serviceCollection, nameof(serviceCollection));
             Ensure.IsNotNull(host, nameof(host));
             Ensure.IsNotNull(implementationFactory, nameof(implementationFactory));
 
-            return AddCustomNClient(serviceCollection, new Uri(host), implementationFactory);
+            return AddCustomNClient(serviceCollection, new Uri(host), implementationFactory, serviceLifetime);
         }
         
         /// <summary>Adds a NClient client to the DI container.</summary>
         /// <param name="serviceCollection"></param>
         /// <param name="host">The base address of URI used when sending requests.</param>
         /// <param name="implementationFactory">The action to create client with builder.</param>
+        /// <param name="serviceLifetime">Specifies the lifetime of a service in an <see cref="IServiceCollection"/></param>
         /// <typeparam name="TClient">The type of interface used to create the client.</typeparam>
         public static IServiceCollection AddCustomNClient<TClient>(this IServiceCollection serviceCollection, 
-            Uri host, Func<IServiceProvider, INClientApiBuilder<TClient>, TClient> implementationFactory)
+            Uri host, Func<IServiceProvider, INClientApiBuilder<TClient>, TClient> implementationFactory,
+            ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
             where TClient : class
         {
             Ensure.IsNotNull(serviceCollection, nameof(serviceCollection));
             Ensure.IsNotNull(host, nameof(host));
             Ensure.IsNotNull(implementationFactory, nameof(implementationFactory));
             
-            return serviceCollection.AddSingleton(serviceProvider => implementationFactory(serviceProvider, new NClientBuilder().For<TClient>(host)));
+            var serviceDescriptor = new ServiceDescriptor(typeof(TClient), serviceProvider => implementationFactory(serviceProvider, new NClientBuilder().For<TClient>(host)), serviceLifetime);
+            serviceCollection.Add(serviceDescriptor);
+            return serviceCollection;
         }
         
         /// <summary>Adds a NClient client to the DI container.</summary>
         /// <param name="serviceCollection"></param>
         /// <param name="implementationFactory">The action to create client with builder.</param>
+        /// <param name="serviceLifetime">Specifies the lifetime of a service in an <see cref="IServiceCollection"/></param>
         /// <typeparam name="TClient">The type of interface used to create the client.</typeparam>
         public static IServiceCollection AddCustomNClient<TClient>(this IServiceCollection serviceCollection, 
-            Func<INClientBuilder, TClient> implementationFactory)
+            Func<INClientBuilder, TClient> implementationFactory,
+            ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
             where TClient : class
         {
             Ensure.IsNotNull(serviceCollection, nameof(serviceCollection));
             Ensure.IsNotNull(implementationFactory, nameof(implementationFactory));
             
-            return serviceCollection.AddSingleton(_ => implementationFactory(new NClientBuilder()));
+            var serviceDescriptor = new ServiceDescriptor(typeof(TClient), _ => implementationFactory(new NClientBuilder()), serviceLifetime);
+            serviceCollection.Add(serviceDescriptor);
+            return serviceCollection;
         }
         
         /// <summary>Adds a NClient client to the DI container.</summary>
         /// <param name="serviceCollection"></param>
         /// <param name="implementationFactory">The action to create client with builder.</param>
+        /// <param name="serviceLifetime">Specifies the lifetime of a service in an <see cref="IServiceCollection"/></param>
         /// <typeparam name="TClient">The type of interface used to create the client.</typeparam>
         public static IServiceCollection AddCustomNClient<TClient>(this IServiceCollection serviceCollection,
-            Func<IServiceProvider, INClientBuilder, TClient> implementationFactory)
+            Func<IServiceProvider, INClientBuilder, TClient> implementationFactory,
+            ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
             where TClient : class
         {
             Ensure.IsNotNull(serviceCollection, nameof(serviceCollection));
             Ensure.IsNotNull(implementationFactory, nameof(implementationFactory));
             
-            return serviceCollection.AddSingleton(serviceProvider => implementationFactory(serviceProvider, new NClientBuilder()));
+            var serviceDescriptor = new ServiceDescriptor(typeof(TClient), serviceProvider => implementationFactory(serviceProvider, new NClientBuilder()), serviceLifetime);
+            serviceCollection.Add(serviceDescriptor);
+            return serviceCollection;
         }
     }
 }
