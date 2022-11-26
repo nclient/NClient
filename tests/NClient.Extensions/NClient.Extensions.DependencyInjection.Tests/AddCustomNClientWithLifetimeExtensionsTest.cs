0a96c55525863e1e893e27b43d47acb9b9b16276
KingMidas74 <kingmidas1992@gmail.com>
2022-11-27 02:51:07 +0500

    add tests for check custom nclient lifetime

---
+++ tests/NClient.Extensions/NClient.Extensions.DependencyInjection.Tests/AddCustomNClientWithLifetimeExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NClient.Extensions.DependencyInjection.Tests.Helpers;
+using NClient.Testing.Common.Apis;
+using NClient.Testing.Common.Clients;
+using NUnit.Framework;
+
+namespace NClient.Extensions.DependencyInjection.Tests
+{
+    [Parallelizable]
+    public class AddCustomNClientWithLifetimeExtensionsTest
+    {
+        [Test]
+        public void AddCustomNClient_WithLifetime_NotThrow([Values] ServiceLifetime serviceLifetime)
+        {
+            const int id = 1;
+            using var api = BasicApiMockFactory.MockGetMethod(id);
+            var serviceCollection = new ServiceCollection();
+            
+            serviceCollection.AddCustomNClient<IBasicClientWithMetadata>(
+                host: api.Urls.First(),
+                implementationFactory: builder => builder
+                    .UsingRestApi()
+                    .UsingHttpTransport()
+                    .UsingJsonSerializer()
+                    .Build(),
+                serviceLifetime);
+
+            serviceCollection.ServiceRegistered<IBasicClientWithMetadata>(serviceLifetime).Should().BeTrue();
+        }
+    }
+}
