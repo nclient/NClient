ef135773959b1cc8fb1040932d633769ded9f512
KingMidas74 <kingmidas1992@gmail.com>
2022-11-27 02:56:44 +0500

    add tests for restNclient extensions with lifetime

---
+++ tests/NClient.Extensions/NClient.Extensions.DependencyInjection.Tests/AddRestNClientWithLifetimeExtensionsTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NClient.Extensions.DependencyInjection.Tests.Helpers;
+using NClient.Testing.Common.Apis;
+using NClient.Testing.Common.Clients;
+using NUnit.Framework;
+
+namespace NClient.Extensions.DependencyInjection.Tests
+{
+    [Parallelizable]
+    public class AddRestNClientWithLifetimeExtensionsTest
+    {
+        [Test]
+        public void AddRestNClient_WithLifetime_NotThrow([Values] ServiceLifetime serviceLifetime)
+        {
+            const int id = 1;
+            using var api = BasicApiMockFactory.MockGetMethod(id);
+            var serviceCollection = new ServiceCollection();
+            
+            serviceCollection.AddRestNClient<IBasicClientWithMetadata>(host: api.Urls.First(), serviceLifetime);
+            
+            serviceCollection.ServiceRegistered<IBasicClientWithMetadata>(serviceLifetime).Should().BeTrue();
+        }
+    }
+}
